653a2d1045c9494766da89420d71dc2b6ce111d8
umerstunner <umerways@gmail.com>
2026-06-13 10:03:06 +0500

    navbar dropdownmenu issues fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,73 +1,94 @@
+"use client";
 import { Briefcase, Droplet } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { getSession } from "@/lib/auth/auth";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { getSession, signOut } from "@/lib/auth/auth";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "./ui/avatar";
+import SignOutBtn from "./sign-out-btn";
+import { useSession } from "@/lib/auth/auth-client";
 
-export default async function Navbar(){
-    const session = await getSession();
-    return (
-			<nav className="border-b border-gray-200 bg-white">
-				<div className="container mx-auto flex h-16 items-center px-4 justify-between">
-					<Link
-						href="/"
-						className="flex items-center gap-2 text-xl font-semibold text-primary"
-					>
-						<Briefcase />
-						Job tracker
-					</Link>
-					<div className="flex items-center gap-4">
-						{session?.user ? (
-							<>
-								<Link href="/dashboard">
-									<Button
-										variant="ghost"
-										className="text-gray-700 hover:text-black"
-									>
-										Dashboard
-									</Button>
-								</Link>
-								<DropdownMenu>
-									<DropdownMenuTrigger>
-										<Button variant="ghost" className="text-gray-700 hover:text-black">
+export default function Navbar() {
+	// const session = await getSession();
+	const { data: session } = useSession();
+	return (
+		<nav className="border-b border-gray-200 bg-white">
+			<div className="container mx-auto flex h-16 items-center px-4 justify-between">
+				<Link
+					href="/"
+					className="flex items-center gap-2 text-xl font-semibold text-primary"
+				>
+					<Briefcase />
+					Job tracker
+				</Link>
+				<div className="flex items-center gap-4">
+					{session?.user ? (
+						<>
+							<Link href="/dashboard">
+								<Button
+									variant="ghost"
+									className="text-gray-700 hover:text-black"
+								>
+									Dashboard
+								</Button>
+							</Link>
+							<DropdownMenu>
+								<DropdownMenuTrigger
+									render={(props) => (
+										<Button
+											{...props}
+											variant="ghost"
+											className="text-gray-700 hover:text-black"
+										>
 											<Avatar>
 												<AvatarFallback className="bg-primary text-white">
 													{session.user.name[0].toUpperCase()}
 												</AvatarFallback>
 											</Avatar>
 										</Button>
-									</DropdownMenuTrigger>
-									<DropdownMenuContent>
-										<DropdownMenuLabel>
-											<div>
-												<p>{session.user.name}</p>
-												<p>{session.user.email}</p>
-											</div>
-										</DropdownMenuLabel>
-										<DropdownMenuItem>Log Out</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</>
-						) : (
-							<>
-								<Link href="/sign-in">
-									<Button
-										variant="ghost"
-										className="text-gray-700 hover:text-black"
-									>
-										Log In
-									</Button>
-								</Link>
-								<Link href="/sign-up" className="">
-									<Button className="bg-primary hover:bg-primary/90 ">
-										Start for free
-									</Button>
-								</Link>
-							</>
-						)}
-					</div>
+									)}
+								/>
+
+								<DropdownMenuContent>
+									<DropdownMenuGroup>
+
+									<DropdownMenuLabel>
+										<div>
+											<p>{session.user.name}</p>
+											<p>{session.user.email}</p>
+										</div>
+									</DropdownMenuLabel>
+									</DropdownMenuGroup>
+									<SignOutBtn />
+								</DropdownMenuContent>
+							</DropdownMenu>
+						</>
+					) : (
+						<>
+							<Link href="/sign-in">
+								<Button
+									variant="ghost"
+									className="text-gray-700 hover:text-black"
+								>
+									Log In
+								</Button>
+							</Link>
+							<Link href="/sign-up" className="">
+								<Button className="bg-primary hover:bg-primary/90 ">
+									Start for free
+								</Button>
+							</Link>
+						</>
+					)}
 				</div>
-			</nav>
-		);
+			</div>
+		</nav>
+	);
 }
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
@@ -20,4 +21,14 @@ export async function getSession() {
         headers: await headers()
     })
     return result;
+}
+
+
+export async function signOut() {
+    const result = await auth.api.signOut({
+        headers: await headers()
+    })
+    if (result.success) {
+        redirect("/sign-in");
+    }
 }
