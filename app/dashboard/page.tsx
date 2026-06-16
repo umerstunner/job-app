989e13e44d0463d6ed9a955d11aa1f382dfe1d59
umerstunner <umerways@gmail.com>
2026-06-16 10:18:43 +0500

    none

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,32 +4,31 @@ import connectDB from "@/lib/db";
 import { Board } from "@/lib/models";
 import { redirect } from "next/navigation";
 
-export default async function Dashboard(){
-    const session = await getSession();
+export default async function Dashboard() {
+	const session = await getSession();
 
-    if(!session?.user) {
-        redirect("/sign-in");
-    }
+	if (!session?.user) {
+		redirect("/sign-in");
+	}
 
-    await connectDB();
+	await connectDB();
 
-    const board = await Board.findOne({
-			userId: session.user.id,
-			name: "Job Hunt",
-		}).populate({
-			path: "columns",
-		});
+	const board = await Board.findOne({
+		userId: session.user.id,
+		name: "Job Hunt",
+	}).populate({
+		path: "columns",
+	});
 
-    
-    return (
-			<div className="min-h-screen bg-white">
-				<div className="container mx-auto p-6">
-					<div className="mb-6">
-						<h1 className="text-3xl font-bold text-black">Job Hunt</h1>
-						<p className="text-gray-600">Track your job applications</p>
-                        <KanbanBoard board={board} userId = {session.user.id}/>
-					</div>
+	return (
+		<div className="min-h-screen bg-white">
+			<div className="container mx-auto p-6">
+				<div className="mb-6">
+					<h1 className="text-3xl font-bold text-black">Job Hunt</h1>
+					<p className="text-gray-600">Track your job applications</p>
+					<KanbanBoard board={board} userId={session.user.id} />
 				</div>
 			</div>
-		);
+		</div>
+	);
 }
